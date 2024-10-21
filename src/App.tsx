9d5eca9a74b20d342d3a781cc0e7eb2f6e12f0a8
Gabriel Lucas <bielfefi@outlook.com>
2024-10-21 14:05:18 -0300

    Componentização das Seções da página do Produto (36.3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles'
 import Header from './Components/Header'
