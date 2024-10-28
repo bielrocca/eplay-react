70940a30083ef38d2f5cdbe1d8891ceee543eb0a
Gabriel Lucas <bielfefi@outlook.com>
2024-10-28 13:41:47 -0300

    Configurando o Redux (Store, addToCart & Provider)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Provider } from 'react-redux'
+
 import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles'
@@ -5,17 +7,20 @@ import Header from './Components/Header'
 
 import Rotas from './routes'
 import Rodape from './Components/Rodape'
+import { store } from './Store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Rodape />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Rodape />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
